c26ad0dea08ffcb52fe8401da5364430826469c2
wangjunkai830 <44988252+wangjunkai830@users.noreply.github.com>
2019-07-21 12:35:06 +0800

    Update .cpm_run.sh

--- v1.0/.cpm_run.sh
+++ v1.0/.cpm_run.sh
@@ -1,14 +1,14 @@
-# file: .compile.sh
+# file: .cpm_run.sh
 # decription: a file of cppproject_manager_kaliv1.0 which is a simple cpp proector manager.
 #             usged to run cpp project
-# usage: sh .run.sh $project_name
+# usage: sh .cpm_run.sh $project_name
 # author: freely
 # date: 2019.07.21
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 if [ "$#" -ne "1" ];
 then
-    echo "usage: sh .cpm_project.sh \$project_name"
+    echo "usage: sh .cpm_run.sh \$project_name"
     exit 1
 fi
 ./$1/release/$1.out
